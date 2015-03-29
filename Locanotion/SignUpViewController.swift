8bfee723cf948a9c0088ffe6b04535075d1a57d0
williambertrand <William@nat-oitwireless-inside-vapornet100-d-22018.Princeton.EDU>
2015-03-29 18:33:12 -0400

    End of day 1

--- Locanotion/SignUpViewController.swift
+++ Locanotion/SignUpViewController.swift
@@ -10,5 +10,85 @@ import Foundation
 import UIKit
 
 class SignUpViewController: UIViewController {
+    @IBOutlet var nameField: UITextField!
+    @IBOutlet var locField: UITextField!
+    @IBOutlet var SignUpButton: UIButton!
+    @IBOutlet var LogInButton: UIButton!
+    
+    @IBOutlet var errorLabel: UILabel!
+    
+    override func viewDidLoad() {
+        SignUpButton.layer.cornerRadius = 3
+        LogInButton.layer.cornerRadius = 3
+        
+    }
+    
+    @IBAction func logIn() {
+        
+        if nameField.text.isEmpty || locField.text.isEmpty {
+            let alert = UIAlertView()
+            alert.title = "No Text"
+            alert.message = "Please Enter Text In The Box"
+            alert.addButtonWithTitle("Ok")
+            alert.show()
+            return
+        }
+
+        PFUser.logInWithUsernameInBackground(nameField.text, password: "password") {
+            (user: PFUser!, error: NSError!) -> Void in
+            if user != nil {
+                dispatch_async(dispatch_get_main_queue()) {
+                    self.performSegueWithIdentifier("goToMainView", sender: self)
+                }
+            }
+            else {
+                //log in failed
+                NSLog("Not Logged in")
+                let alert = UIAlertView()
+                alert.title = "Error"
+                alert.message = "Invalid Login"
+                alert.addButtonWithTitle("Ok")
+                alert.show()
+                
+            }
+        }
+
+    }
+    
+    @IBAction func signUp() {
+        
+        if nameField.text.isEmpty || locField.text.isEmpty {
+            let alert = UIAlertView()
+            alert.title = "No Text"
+            alert.message = "Please Enter Text In The Box"
+            alert.addButtonWithTitle("Ok")
+            alert.show()
+            return
+        }
+
+        var user = PFUser()
+        user.username = nameField.text
+        user.password = "password"
+        user.email = nameField.text + "@princeton.edu"
+        
+        user["LocationName"] = locField.text
+        
+        user.signUpInBackgroundWithBlock({ (succeeded: Bool!, error: NSError!) -> Void in
+            if error != nil {
+                let alert = UIAlertView()
+                alert.title = "Error"
+                alert.message = "Already a user with that name"
+                alert.addButtonWithTitle("Ok")
+                alert.show()
+            }
+            else {
+                dispatch_async(dispatch_get_main_queue()) {
+                    self.performSegueWithIdentifier("goToMainView", sender: self)
+                }
+            }
+        })
+
+    }
+
 }
 
--- Locanotion/ViewController.swift
+++ Locanotion/ViewController.swift
@@ -13,6 +13,17 @@ class ViewController: UIViewController {
     @IBOutlet weak var ViewMapButton: UIButton!
     @IBOutlet weak var ViewFriendsButton: UIButton!
     @IBOutlet weak var ViewClubsButton: UIButton!
+    @IBOutlet var CurrentUserLabel: UILabel!
+    
+    override func viewWillAppear(animated: Bool) {
+        var currentUser = PFUser.currentUser()
+        if currentUser != nil {
+            CurrentUserLabel.text = currentUser.username
+        }
+        else {
+            CurrentUserLabel.text = "No User"
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
