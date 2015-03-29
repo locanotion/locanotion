95e5b25a75b1a4ef2aad549ba046e8f9447c075e
Nicholas Pai <npai@princeton.edu>
2015-03-29 16:07:14 -0400

    test sign up page

---
+++ Locanotion/SignUpViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SignUpViewController.swift
+//  Locanotion
+//
+//  Created by Nicholas Pai on 3/29/15.
+//  Copyright (c) 2015 Locanotion. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SignUpViewController: UIViewController {
+}
+
